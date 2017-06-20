cc079b4db5b86817307f97ad318ab1b1fba85b03
LoveLeslie <anqiaoli@email.szu.edu.cn>
2017-06-20 15:06:34 +0800

    项目打包

---
+++ prod.server.js
@@ -0,0 +1,70 @@
+var express = require('express')
+var config = require('./config/index')
+var axios = require('axios')
+
+var port = process.env.PORT || config.build.port
+
+var app = express()
+
+var apiRoutes = express.Router()
+
+apiRoutes.get('/toplist', function (req, res) {
+  var url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_opt.fcg'
+
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    var ret = response.data
+    if (typeof ret === 'string') {
+      var matches = ret.slice(14, ret.length - 1)
+      if (matches) {
+        ret = JSON.parse(matches)
+      }
+    }
+    res.json(ret)
+  })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+apiRoutes.get('/lyric', function (req, res) {
+  var url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    var ret = response.data
+    if (typeof ret === 'string') {
+      var reg = /^\w+\(({[^()]+})\)$/
+      var matches = ret.match(reg)
+      if (matches) {
+        ret = JSON.parse(matches[1])
+      }
+    }
+    res.json(ret)
+  })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+app.use('/api', apiRoutes)
+
+app.use(express.static('./dist'))
+
+module.exports = app.listen(port, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('Listening at http://localhost:' + port + '\n')
+})
